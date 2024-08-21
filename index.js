67eebf03815f6966bab002518c6d7762c972cd81
carlos bernal <bernal199152@yahoo.com>
2024-08-21 16:18:05 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 function writeCards(names, event) {
-       let messages = [];
-       for (let i = 0; i < names.length; i++) {
-         messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
-       }
-       return messages;
-     }
+    let messages = [];
+    for (let i = 0; i < names.length; i++) {
+      messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+  }
 
-     function countDown(number) {
-      for (let i =number; i>=0; i--) {
-        console.log(i);
-      }
-     }
+  function countDown(number) {
+   for (let i =number; i>=0; i--) {
+     console.log(i);
+   }
+  }
